182abd6c7e4673527efe6c653eb7fb0a6f98d05e
Siddhi Gawade <siddhigawade.sbg25@gmail.com>
2025-03-05 12:27:24 +0530

    bugfix/fix-Fixed App.jsx added route of landing page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './components/theme-provider';
-import { Provider } from 'react-redux';
-import store from './redux/store/store';
-import { Home } from './pages/Home/Home';
-import { Footer } from './components/Footer';
-import Questions from './pages/Assessments/Questions';
-import RoadmapPage from './pages/Assessments/Roadmap';
-import Dashboard from './pages/Dashboard/Dashboard';
-import Achievements from './pages/Dashboard/Achievements';
-import CourseOverview from './pages/Dashboard/CourseOverview';
-import Internships from './pages/internships/Internships';
-import SignUpLayout from './pages/SignUp/Layout';
-import LogInLayout from './pages/LogIn/Layout';
-import SignUp from './modals/SignUp';
-import Login from './modals/Login';
-import { Navbar } from './components/navbar';
-import CourseLearning from './pages/Dashboard/CourseLearning';
-import ProtectedRoute from './components/ProtectedRoute';
-import  InstructorDashboard  from './pages/Instructor/InstructorDashboard';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom"
+import { ThemeProvider } from "./components/theme-provider"
+import { Navbar } from "./components/Navbar"
+import { Footer } from "./components/Footer"
+import SignUpLayout from "./pages/SignUp/Layout" // Update import path for SignUp Layout component
+import LogInLayout from "./pages/LogIn/Layout" // Import LogIn Layout component
+import TopSection from "./pages/LandingPage/TopSection" // Import TopSection component
 
 function App() {
   return (
@@ -27,22 +14,9 @@ function App() {
         <div className="min-h-screen bg-background text-foreground">
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<SignUpLayout />} />
-            <Route path="/login" element={<LogInLayout />} />
-            <Route path="/internships" element={<Internships />} />
-            <Route path="/instructor" element={<InstructorDashboard />} />
-            
-            
-
-            {/* Protected Routes */}
-            <Route path="/assessment" element={<ProtectedRoute element={<Questions />} />} />
-            <Route path="/roadmap" element={<ProtectedRoute element={<RoadmapPage />} />} />
-            <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
-            {/* <Route path="/instructor" element={<ProtectedRoute element={<InstructorDashboardpage />} />}/> */}
-            <Route path="/achievements" element={<ProtectedRoute element={<Achievements />} />} />
-            <Route path="/course" element={<ProtectedRoute element={<CourseOverview />} />} />
-            <Route path="/learning" element={<ProtectedRoute element={<CourseLearning />} />} />
+            <Route path="/signup" element={<SignUpLayout />} /> {/* Add route for SignUp Layout */}
+            <Route path="/login" element={<LogInLayout />} /> {/* Add route for LogIn Layout */}
+            <Route path="/" element={<TopSection />} /> {/* Add route for TopSection */}
           </Routes>
           <Footer />
         </div>
