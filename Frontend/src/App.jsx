47bfa2b08dbc305877811c5974d7738b9d19b0d1
Siddhi Gawade <siddhigawade.sbg25@gmail.com>
2025-03-05 12:27:24 +0530

    bugfix/fix-Fixed App.jsx added route of landing page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom"
 import { ThemeProvider } from "./components/theme-provider"
 import { Navbar } from "./components/Navbar"
-import { Home } from "./pages/Home"
 import { Footer } from "./components/Footer"
 import SignUpLayout from "./pages/SignUp/Layout" // Update import path for SignUp Layout component
 import LogInLayout from "./pages/LogIn/Layout" // Import LogIn Layout component
+import TopSection from "./pages/LandingPage/TopSection" // Import TopSection component
 
 function App() {
   return (
@@ -14,9 +14,9 @@ function App() {
         <div className="min-h-screen">
           <ConditionalNavbar />
           <Routes>
-            <Route path="/" element={<Home />} />
             <Route path="/signup" element={<SignUpLayout />} /> {/* Add route for SignUp Layout */}
             <Route path="/login" element={<LogInLayout />} /> {/* Add route for LogIn Layout */}
+            <Route path="/" element={<TopSection />} /> {/* Add route for TopSection */}
           </Routes>
           <Footer />
         </div>
